69068a98ba5e7e600d60a632c77801884bd44c14
AwaisChandio45 <awais1@webhr.email>
2026-04-07 17:47:43 +0500

    libraries and other files added

---
+++ src/components/ScreenWrapper.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ScreenWrapper = ()=>{
+
+<SafeAreaView>
+
+    
+</SafeAreaView>
+
+
+}
+export default  ScreenWrapper;
---
+++ src/themes/colors.tsx
@@ -0,0 +1,14 @@
+export const colors = {
+
+    primary: '#2563EB',
+    background: '#F8FAFC',
+    surface: '#FFFFFF',
+    textPrimary: '#0F172A',
+    textSecondary: '#64748B',
+    border: '#E2E8F0',
+    success: '#16A34A',
+    error: '#DC2626',
+    warning: '#F59E0B',
+    white: '#FFFFFF',
+    black: '#000000',
+}
