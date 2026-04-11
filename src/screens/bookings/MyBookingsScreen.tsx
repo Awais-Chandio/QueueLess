ee85a34ecaf5a618c7848c217087869143cfdf49
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-11 20:49:11 +0500

    further api screens added

--- src/screens/bookings/MyBookingsScreen.tsx
+++ src/screens/bookings/MyBookingsScreen.tsx
@@ -1,3 +1,52 @@
-const MyBookingsScreen = () => null;
+import React from "react";
+import { View,StyleSheet,Text } from "react-native";
+import {colors,spacing, typography} from "../../theme";
+import ScreenWrapper from "../../components/common/ScreenWrapper";
+import EmptyState from "../../components/common/EmptyState";
+
+const MyBookingsScreen =()=>{
+    return(
+        <ScreenWrapper>
+            <View
+            style={styles.container}>
+                <Text style={styles.title}>
+                    My Bookings
+                </Text>
+                <Text style={styles.subtitle}>
+                    Your bookings will appear here
+                </Text>
+                <EmptyState
+                title="No bookings yet"
+                subtitle="Your bookings will appear here once you create one."
+                />
+
+                
+
+
+            </View>
+        </ScreenWrapper>
+    )
+}
 
 export default MyBookingsScreen;
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: typography.h1,
+        fontWeight: 'bold',
+        color: colors.text,
+        marginBottom: spacing.sm,
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: typography.body,
+        color: colors.textSecondary,
+        marginBottom: spacing.lg,
+        textAlign: 'center',
+     },
+})
--- src/screens/centers/CentersScreen.tsx
+++ src/screens/centers/CentersScreen.tsx
@@ -1,3 +1,50 @@
-const CentersScreen = () => null;
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { colors, spacing, typography } from "../../theme";
+import ScreenWrapper from "../../components/common/ScreenWrapper";
+import EmptyState from "../../components/common/EmptyState";
 
+const CentersScreen = () => {
+    return (
+        <ScreenWrapper>
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Centers
+                </Text>
+                <Text style={styles.subtitle}>
+                    Available centers will appear here.
+                </Text>
+                <EmptyState
+                    title="No Centers Available"
+                    subtitle="Centers will appear here once data is added"
+                    onButtonPress={() => { }}
+                />
+
+
+            </View>
+
+        </ScreenWrapper>
+
+    )
+}
 export default CentersScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: typography.h1,
+        fontWeight: 'bold',
+        color: colors.text,
+        marginBottom: spacing.sm,
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: typography.body,
+        color: colors.textSecondary,
+        marginBottom: spacing.lg,
+        textAlign: 'center',
+    },
+})
