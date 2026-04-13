5e6971d4e59f158d5ae3f8a3e3d82addaa8f768d
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-13 21:15:17 +0500

    further settings screen added and splashscreen updated

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,3 +1,81 @@
-const SettingsScreen = () => null;
+import React from "react";
+import { View, StyleSheet, Text, Pressable } from "react-native";
+import { colors, spacing, typography,radius } from "../../theme";
+import ScreenWrapper from "../../components/common/ScreenWrapper";
+
+const SettingsScreen = () => {
+    return (
+        <ScreenWrapper>
+            <View style={Styles.container}>
+                <Text style={Styles.title}>
+                    Settings
+                </Text>
+                <Text style={Styles.subtitle}>
+                    Manage your app preferences.
+                </Text>
+                <View style={Styles.optionsContainer}>
+                    <Pressable style={Styles.optionalRow}>
+
+                        <Text style={Styles.optionText}>
+                            Edit Profile
+                        </Text>
+
+
+                    </Pressable >
+                    <Pressable style={Styles.optionalRow}>
+                        <Text style={Styles.optionText}>
+                            Notifications
+                        </Text>
+
+
+                    </Pressable>
+                    <Pressable style={Styles.optionalRow}>
+                        <Text style={Styles.optionText}>
+                            Logout
+                        </Text>
+
+
+                    </Pressable>
+                </View>
+            </View>
+        </ScreenWrapper>
+    )
+}
 
 export default SettingsScreen;
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: spacing.lg,
+    },
+    title: {
+        fontSize: typography.h1,
+        fontWeight: 'bold',
+        color: colors.text,
+        marginBottom: spacing.sm,
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: typography.body,
+        color: colors.textSecondary,
+        marginBottom: spacing.lg,
+        textAlign: 'center',
+    },
+    optionsContainer: {
+        backgroundColor: colors.surface,
+        borderRadius: radius.borderRadius,
+        padding: spacing.lg,
+        borderWidth: 1,
+        borderColor: colors.border,
+    },
+    optionalRow: {
+        paddingVertical: spacing.sm,    
+        borderBottomColor: colors.border,
+    },
+    optionText: {
+        fontSize: typography.body,
+        color: colors.text,
+    },
+})
--- src/screens/splash/SplashScreen.tsx
+++ src/screens/splash/SplashScreen.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import {View,Text,StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import ScreenWrapper from "../../components/common/ScreenWrapper";
 import Loader from "../../components/common/Loader";
+import { colors, spacing, typography } from "../../theme";
 
-const SplashScreen=()=>{
-    return(
+const SplashScreen = () => {
+    return (
 
         <ScreenWrapper>
 
             <View style={Styles.container} >
-                <Loader>
+                <Text style={Styles.title} >
+                    QueueLess
+                </Text>
+                <Text style={Styles.subTitle} >
+                    Managing your queues efficiently
+                </Text>
 
-                    
-                </Loader>
+                <Loader
+                
+                />
+
+
+
 
             </View>
         </ScreenWrapper>
@@ -21,4 +31,22 @@ const SplashScreen=()=>{
 export default SplashScreen
 const Styles = StyleSheet.create({
 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: typography.h1,
+        marginBottom: spacing.xl,
+        color: colors.primary,
+        fontWeight: 'bold',
+
+    },
+    subTitle: {
+        fontSize: typography.body,
+        color: colors.textSecondary,
+        marginBottom: spacing.xl,
+
+    }
 })
