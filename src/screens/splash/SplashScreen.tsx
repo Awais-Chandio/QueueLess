32f5a9bb1d07d5aef3ce81926a1a2a222be66578
AwaisChandio45 <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-13 18:01:10 +0500

    splash screen added

--- src/screens/splash/SplashScreen.tsx
+++ src/screens/splash/SplashScreen.tsx
@@ -1,3 +1,24 @@
-const SplashScreen = () => null;
+import React from "react";
+import {View,Text,StyleSheet} from "react-native";
+import ScreenWrapper from "../../components/common/ScreenWrapper";
+import Loader from "../../components/common/Loader";
 
-export default SplashScreen;
+const SplashScreen=()=>{
+    return(
+
+        <ScreenWrapper>
+
+            <View style={Styles.container} >
+                <Loader>
+
+                    
+                </Loader>
+
+            </View>
+        </ScreenWrapper>
+    )
+}
+export default SplashScreen
+const Styles = StyleSheet.create({
+
+})
