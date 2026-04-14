453241343749c97d5d0edef06b449e0003fd1179
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-14 23:41:35 +0500

    Conditional AuthLogin added

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AuthNavigator from "./AuthNavigator";
+import AppTabs from "./AppTabs";
+
+const RootNavigator=()=>{
+
+     const isLoggedIn =false;
+
+    return(
+        
+
+            isLoggedIn ? <AppTabs/> : <AuthNavigator/>
+      
+
+    )
+}
+export default RootNavigator;
