217c94029fe1bbe5d3c7756c38f015f8fe684f67
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-14 23:24:22 +0500

    Added AppTabs or bottom tabs

--- src/navigation/AppTabs.tsx
+++ src/navigation/AppTabs.tsx
@@ -1,3 +1,27 @@
-const AppTabs = () => null;
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/home/HomeScreen";
+import SettingsScreen from "../screens/settings/SettingsScreen";
+import CentersScreen from "../screens/centers/CentersScreen";
+import MyBookingsScreen from "../screens/bookings/MyBookingsScreen";
+import ProfileScreen from "../screens/profile/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+const AppTabs = ()=>{
+    return(
+
+        <Tab.Navigator
+        screenOptions={{headerShown:false}}>
+        <Tab.Screen name ="Home" component={HomeScreen}/>
+        <Tab.Screen name = "Centers" component={CentersScreen}/>
+        <Tab.Screen name = "My Bookings" component={MyBookingsScreen}/>
+        <Tab.Screen name = "Profile" component={ProfileScreen}/>
+        <Tab.Screen name = "Settings" component={SettingsScreen}/>   
+
+
+        </Tab.Navigator>
+    )
+}
 
 export default AppTabs;
