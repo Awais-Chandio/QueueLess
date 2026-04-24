11d83051ae8107e098bffba0f802376efdafca46
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-24 22:57:20 +0500

    useAuth updated

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,3 +2,24 @@ import {authService } from "../services/auth/authService"
 import {useAuthStore} from "../store/authStore"
 import {LoginPayload,SignupPayload} from "../types/auth"
  
+export const useAuth = () => {
+  const {
+    session,
+    user,
+    isAuthenticated,
+    isLoading,
+    setAuth,
+    clearAuth,
+    setLoading
+  }  = useAuthStore();
+
+  return {
+    session,
+    user,
+    isAuthenticated,
+    isLoading,
+    setAuth,
+    clearAuth,
+    setLoading
+  }
+}
