8cfd98547fdb09305f1f1d5fb89b1fde80d3888a
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2026-04-27 22:29:55 +0500

    AppNavigato updated

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppTabs from "./AppTabs";
+import CenterDetailsScreen from "../screens/centers/CenterDetailsScreen";
+import BookAppointmentScreen from "../screens/bookings/BookAppointmentScreen";
+import BookingDetailsScreen from "../screens/bookings/BookingDetailsScreen";
+import QueueStatusScreen from "../screens/bookings/QueueStatusScreen";
+import EditProfileScreen from "../screens/profile/EditProfileScreen";
+import type { AppStackParamList } from "./types";
+
+const Stack = createNativeStackNavigator<AppStackParamList>();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="MainTabs" component={AppTabs} />
+      <Stack.Screen name="CenterDetails" component={CenterDetailsScreen} />
+      <Stack.Screen name="BookAppointment" component={BookAppointmentScreen} />
+      <Stack.Screen name="BookingDetails" component={BookingDetailsScreen} />
+      <Stack.Screen name="QueueStatus" component={QueueStatusScreen} />
+      <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
